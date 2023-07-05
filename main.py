3c387555b7cdb96631ad5981adaf56f209d475c5
Thinline20 <thinline20@gmail.com>
2023-07-05 16:57:23 +0900

    Add readme

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-import gradio
+import gradio as gr
+
+def greet(name: str):
+    return f"Hello {name}!"
+
+demo = gr.Interface(fn=greet, inputs=gr.Textbox(lines=2, placeholder="Name"), outputs="text")
+
+demo.launch()
